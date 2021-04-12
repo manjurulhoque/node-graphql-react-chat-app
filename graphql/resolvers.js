5f4d93db97aad8b087cf7b2986522fa89fcf465f
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-04-13 01:13:55 +0600

    Initial commit

---
+++ graphql/resolvers.js
@@ -0,0 +1,114 @@
+const { User } = require('../models');
+const bcrypt = require('bcryptjs');
+const { UserInputError, AuthenticationError } = require('apollo-server');
+const jwt = require('jsonwebtoken');
+const { Op } = require('sequelize');
+
+const { JWT_SECRET } = require('../config/env.json')
+
+module.exports = {
+    Query: {
+        getUsers: async () => {
+            const users = await User.findAll();
+            return users;
+        },
+        login: async (parent, args, context, info) => {
+            const { username, password } = args;
+            let errors = {};
+
+            try {
+                if (username.trim() === '')
+                    errors.username = 'username can not be empty';
+                if (password === '') errors.password = 'password can not be empty';
+
+                if (Object.keys(errors).length > 0) {
+                    throw new UserInputError('bad input', { errors });
+                }
+
+                const user = await User.findOne({
+                    where: { username },
+                });
+
+                if (!user) {
+                    errors.username = 'user not found';
+                    throw new UserInputError('user not found', { errors });
+                }
+
+                const correctPassword = await bcrypt.compare(password, user.password);
+
+                if (!correctPassword) {
+                    errors.password = 'password is incorrect';
+                    throw new AuthenticationError('password is incorrect', { errors });
+                }
+
+                const token = jwt.sign({ username }, JWT_SECRET, {
+                    expiresIn: 60 * 60,
+                });
+
+                return {
+                    ...user.toJSON(),
+                    createdAt: user.createdAt.toISOString(),
+                    token,
+                };
+            } catch (err) {
+                console.log(err);
+                throw err;
+            }
+        }
+    },
+    Mutation: {
+        register: async (parent, args, context, info) => {
+            let { username, email, password, confirmPassword } = args;
+
+            let errors = {};
+
+            try {
+                // Validate input data
+                if (email.trim() === '') errors.email = 'email must not be empty'
+                if (username.trim() === '')
+                    errors.username = 'username must not be empty'
+                if (password.trim() === '')
+                    errors.password = 'password must not be empty'
+                if (confirmPassword.trim() === '')
+                    errors.confirmPassword = 'repeat password must not be empty'
+
+                if (password !== confirmPassword)
+                    errors.confirmPassword = 'passwords must match'
+
+                // // Check if username / email exists
+                // const userByUsername = await User.findOne({ where: { username } })
+                // const userByEmail = await User.findOne({ where: { email } })
+
+                // if (userByUsername) errors.username = 'Username is already taken';
+                // if (userByEmail) errors.email = 'Email is already taken';
+
+                if (Object.keys(errors).length > 0) {
+                    throw errors
+                }
+
+                // Hash password
+                password = await bcrypt.hash(password, 6)
+
+                // Create user
+                const user = await User.create({
+                    username,
+                    email,
+                    password,
+                })
+
+                // Return user
+                return user
+            } catch (err) {
+                console.log(err)
+                if (err.name === 'SequelizeUniqueConstraintError') {
+                    err.errors.forEach(
+                        (e) => (errors[e.path] = `${e.path} is already taken`)
+                    )
+                } else if (err.name === 'SequelizeValidationError') {
+                    err.errors.forEach((e) => (errors[e.path] = e.message))
+                }
+                throw new UserInputError('Bad input', { errors })
+            }
+        }
+    }
+};
