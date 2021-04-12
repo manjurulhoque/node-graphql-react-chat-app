ee108c1080de96e77ca97aea6d3fce6140b6f560
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2021-04-13 01:31:14 +0600

    Get all users

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -8,9 +8,30 @@ const { JWT_SECRET } = require('../config/env.json')
 
 module.exports = {
     Query: {
-        getUsers: async () => {
-            const users = await User.findAll();
-            return users;
+        getUsers: async (parent, args, context, info) => {
+            try {
+                let user
+                if (context.req && context.req.headers.authorization) {
+                    const token = context.req.headers.authorization.split('Bearer ')[1]
+                    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+                        if (err) {
+                            throw new AuthenticationError('Unauthenticated')
+                        }
+                        user = decodedToken
+                    })
+                } else {
+                    throw new AuthenticationError('Unauthenticated')
+                }
+
+                const users = await User.findAll({
+                    where: { username: { [Op.ne]: user.username } },
+                })
+
+                return users
+            } catch (err) {
+                console.log(err)
+                throw err
+            }
         },
         login: async (parent, args, context, info) => {
             const { username, password } = args;
